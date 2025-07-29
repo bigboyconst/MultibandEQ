070bf6d3ec0758d35716d7fd6075f9ca3f0a7adb
bigboyconst <constantinbrouillard4@gmail.com>
2025-07-29 10:30:19 -0400

    Edit formatting to my liking

--- MultibandEQ.h
+++ MultibandEQ.h
@@ -6,8 +6,8 @@ const int kNumPresets = 1;
 
 enum EParams
 {
-  kGain = 0,
-  kNumParams
+    kGain = 0,
+    kNumParams
 };
 
 using namespace iplug;
@@ -15,10 +15,10 @@ using namespace igraphics;
 
 class MultibandEQ final : public Plugin
 {
-public:
-  MultibandEQ(const InstanceInfo& info);
+  public:
+    MultibandEQ(const InstanceInfo& info);
 
 #if IPLUG_DSP // http://bit.ly/2S64BDd
-  void ProcessBlock(sample** inputs, sample** outputs, int nFrames) override;
+    void ProcessBlock(sample** inputs, sample** outputs, int nFrames) override;
 #endif
 };
